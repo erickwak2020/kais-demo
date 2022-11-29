fd39d6f4b7b9ee1297f3cc0df61d7ba5903d1fcf
곽정환 <erickwak2020@gmail.com>
2022-11-29 13:56:41 +0900

    vue 빌드시 springboot와 연동하도록 설정

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: "../src/main/resources/static",
+  devServer: {
+    proxy: {
+      '/api': {
+        // '/api' 로 들어오면 포트 9090(스프링 서버)로 보낸다
+        target: 'http://localhost:9090',
+        changeOrigin: true // cross origin 허용
+      }
+    }
+  }
 })
